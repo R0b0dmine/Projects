d2859de62e7fde164999af35defce7b9155d75fe
Dominik <panasz.d@gmail.com>
2018-11-24 13:56:46 +0100

    WEWE

---
+++ html-course/onas/onas.js
@@ -0,0 +1,10 @@
+function  dlaONAs(){
+document.getElementById('opis').innerHTML = 'O nas'; }
+function tyk(){
+document.getElementById('yeet').innerHTML = '<h1>CHEW</h1>';}
+function rre(){
+document.getElementById('well').innerHTML = '<p>Stop right there criminal scum, pay the court a fine or serve your sentence, Your stolen goods are now forfeit </p>'}
+function ttt(){
+document.getElementById('gl').innerHTML ='<h1>Michał</h1>';}	
+		
+

e06e9680942b51e0349c79cf637508d92e62037e
Dominik <panasz.d@gmail.com>
2018-12-15 13:46:21 +0100

    yeet

--- PixelArt/script.js
+++ PixelArt/script.js
@@ -1,5 +1,6 @@
 
 var penColour = 'black';
+var isHeader = 0
 function setPixelColour(pixel)
 {
  pixel.style.backgroundColor = penColour;
@@ -14,6 +15,34 @@ function setCurrentColour(colour)
 {
 	var currentColour = document.getElementById("currentColour");
 	currentColour.style.backgroundColor = colour;
+	
+	
+}
+function setText()
+{
+	if (isHeader == 0)
+	{
+	document.getElementById("palette").innerHTML = '<h1 id="pe">Paleta</h1>' + document.getElementById("palette").innerHTML; 
+	isHeader = 1;
+     }
+	else {
+		elem = document.getElementById("pe");
+		elem.parentNode.removeChild(elem);
+	isHeader = 0;
+	}
+}
+
+
+
+function addRow()
+{
+	var rowCollection = document.getElementsByClassName("row");
+	document.getElementById("bigartsquare").innerHTML = document.getElementById("bigartsquare").innerHTML + '<div class="row">' + rowCollection[0].innerHTML + '</div>' ;
+}
+function removeRow()
+{ 
+	var rowCollection = document.getElementsByClassName("row");
+	 rowCollection[0].remove();
 	
 	
 }

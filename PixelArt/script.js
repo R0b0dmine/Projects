e3f3bbeb6b8d811286525b8107082b92fb077d6c
Dominik <panasz.d@gmail.com>
2018-12-22 14:36:25 +0100

    yeet

--- PixelArt/script.js
+++ PixelArt/script.js
@@ -1,6 +1,11 @@
 
 var penColour = 'black';
-var isHeader = 0
+var isHeader = 0;
+const emptyRow = '<div class="row"></div>';
+const emptyColumn = '<div class="pixel" onclick="setPixelColour(this)"></div>';
+
+
+
 function setPixelColour(pixel)
 {
  pixel.style.backgroundColor = penColour;
@@ -37,12 +42,52 @@ function setText()
 function addRow()
 {
 	var rowCollection = document.getElementsByClassName("row");
-	document.getElementById("bigartsquare").innerHTML = document.getElementById("bigartsquare").innerHTML + '<div class="row">' + rowCollection[0].innerHTML + '</div>' ;
+	document.getElementById("bigartsquare").innerHTML = document.getElementById("bigartsquare").innerHTML + emptyRow ;
 }
 function removeRow()
 { 
 	var rowCollection = document.getElementsByClassName("row");
-	 rowCollection[0].remove();
-	
+	var count = rowCollection.length;
+	 rowCollection[count-1].remove();
+}
+
+function addRowsAtLoad()
+{
+	var i,j;
+	for (i=0; i<8; i++)
+	{
+		document.getElementById("bigartsquare").innerHTML = document.getElementById("bigartsquare").innerHTML +  emptyRow;
+		var rowCollection = document.getElementsByClassName("row");
+		for(j=0;j<8; j++)
+		{
+			rowCollection[i].innerHTML = rowCollection[i].innerHTML + emptyColumn;
+		}
+	}
 	
 }
+
+function addColumn()
+{
+	var j;
+	var rowCollection = document.getElementsByClassName("row");
+	var count = rowCollection.length;
+	document.getElementById("bigartsquare").innerHTML = document.getElementById("bigartsquare").innerHTML ;
+	for(j=0;j<count; j++)
+		{
+			rowCollection[j].innerHTML = rowCollection[j].innerHTML + emptyColumn;
+		}
+}
+function removeColumn()
+{
+	var rowCollection = document.getElementsByClassName("row");
+	var rowCounter = rowCollection.length;
+	var columnCollection = document.getElementsByClassName("pixel");
+	var columnCounter = columnCollection.length;
+	 
+	
+}
+
+
+
+
+
